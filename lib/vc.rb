e12a834fed5ea4d4a9cd7e2d9ab3b53b22cde862
Thomas Carr <9591402+htcarr3@users.noreply.github.com>
2023-03-10 18:19:13 -0500

    first commit

---
+++ lib/vc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "vc/version"
+
+module VC
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/vc_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe VC do
+  it "has a version number" do
+    expect(VC::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
